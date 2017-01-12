55bdead29c3ed887656cae7c0c47d3ebc36e485c
Sara Jensen <jensen.sara.e@gmail.com>
2017-01-11 20:27:23 -0800

    Refactor to move facts and the fact finding function to their own FactBook class.

--- app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
@@ -6,9 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 public class FunFactsActivity extends AppCompatActivity implements View.OnClickListener {
+    private FactBook mFactBook = new FactBook();
     // Declare view variables
     private TextView mFactTextView;
     private Button mShowFactButton;
@@ -30,24 +29,7 @@ public class FunFactsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         // The button was clicked, so update the fact TextView with a new fact
         if (v == mShowFactButton) {
-            String[] facts = {
-                    "Ants stretch when they wake up in the morning.",
-                    "Ostriches can run faster than horses.",
-                    "Olympic gold medals are actually made mostly of silver.",
-                    "You are born with 300 bones; by the time you are an adult you will have 206.",
-                    "It takes about 8 minutes for light from the Sun to reach Earth.",
-                    "Some bamboo plants can grow almost a meter in just one day.",
-                    "The state of Florida is bigger than England.",
-                    "Some penguins can leap 2-3 meters out of the water.",
-                    "On average, it takes 66 days to form a new habit.",
-                    "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-            String fact = "";
-            // Randomly select a fact
-            Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.nextInt(facts.length);
-            fact = facts[randomNumber];
-
+            String fact = mFactBook.getFact();
             // Update the screen with our new dynamic fact
             mFactTextView.setText(fact);
         }
