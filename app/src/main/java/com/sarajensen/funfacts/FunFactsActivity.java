111d737d8c9fa6f119878bf3b42d3e2105ea5c05
Sara Jensen <jensen.sara.e@gmail.com>
2017-01-11 20:08:54 -0800

    Show a fact (of a set of 10 facts) to the user at random when they click 'Show Another Fun Fact.

--- app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
@@ -30,11 +30,23 @@ public class FunFactsActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         // The button was clicked, so update the fact TextView with a new fact
         if (v == mShowFactButton) {
+            String[] facts = {
+                    "Ants stretch when they wake up in the morning.",
+                    "Ostriches can run faster than horses.",
+                    "Olympic gold medals are actually made mostly of silver.",
+                    "You are born with 300 bones; by the time you are an adult you will have 206.",
+                    "It takes about 8 minutes for light from the Sun to reach Earth.",
+                    "Some bamboo plants can grow almost a meter in just one day.",
+                    "The state of Florida is bigger than England.",
+                    "Some penguins can leap 2-3 meters out of the water.",
+                    "On average, it takes 66 days to form a new habit.",
+                    "Mammoths still walked the earth when the Great Pyramid was being built." };
+
             String fact = "";
             // Randomly select a fact
             Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.nextInt(3);
-            fact = randomNumber + "";
+            int randomNumber = randomGenerator.nextInt(facts.length);
+            fact = facts[randomNumber];
 
             // Update the screen with our new dynamic fact
             mFactTextView.setText(fact);
