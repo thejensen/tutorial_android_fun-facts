6679997d9881e043a01f19e884e2192ac012d974
Sara Jensen <jensen.sara.e@gmail.com>
2017-01-11 18:32:13 -0800

    Change the fact to a randomly generated number between 0 and 3 when the 'show another fact' button is clicked.

--- app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
@@ -1,13 +1,43 @@
 package com.sarajensen.funfacts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class FunFactsActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity implements View.OnClickListener {
+    // Declare view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Assign the views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        mShowFactButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // The button was clicked, so update the fact TextView with a new fact
+        if (v == mShowFactButton) {
+            String fact = "";
+            // Randomly select a fact
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.nextInt(3);
+            fact = randomNumber + "";
+
+            // Update the screen with our new dynamic fact
+            mFactTextView.setText(fact);
+        }
     }
 }
