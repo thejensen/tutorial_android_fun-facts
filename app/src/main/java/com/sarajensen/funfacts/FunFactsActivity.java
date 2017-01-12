ea7c59791c97bfd59177dd23bb4bc7cc4a0553cc
Sara Jensen <jensen.sara.e@gmail.com>
2017-01-11 20:55:35 -0800

    Change the background color at random from the list of colors in the ColorWheel class. Also change the button text color to match the randomly selected background color.

--- app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sarajensen/funfacts/FunFactsActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity implements View.OnClickListener {
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
     // Declare view variables
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +24,20 @@ public class FunFactsActivity extends AppCompatActivity implements View.OnClickL
         // Assign the views from the layout file to the corresponding variables
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_fun_facts);
 
         mShowFactButton.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
         // The button was clicked, so update the fact TextView with a new fact
         if (v == mShowFactButton) {
             String fact = mFactBook.getFact();
+            int color = mColorWheel.getColor();
+            mRelativeLayout.setBackgroundColor(color);
+            mShowFactButton.setTextColor(color);
+
             // Update the screen with our new dynamic fact
             mFactTextView.setText(fact);
         }
